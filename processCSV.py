df7ff85b9ba3b1659fec6a49e79c8e5e2468003a
Brqndon1 <bmlyubar@syr.edu>
2025-10-27 00:23:09 -0400

    download button

--- processCSV.py
+++ processCSV.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, send_file
 from flask_cors import CORS
 import pandas as pd
 
@@ -15,7 +15,6 @@ def upload():
 
     stroke = request.form.get("stroke")
     ageGroup = request.form.get("ageGroup")
-    number = request.form.get("number")
     fastest3 = request.form.get("fastest3")
 
     if stroke:
@@ -27,9 +26,20 @@ def upload():
     if fastest3:
         df = df.sort_values("original_time").head(3)
 
-    print(df) 
+    # makes df into excel format 
+    # file is ready to be downloaded later in /upload
+    df.to_excel('temp_results.xlsx', index=False)
+
     back_button = '<button onclick="history.back()">Back</button>'
+    download_button = '<a href="/download_excel"><button>Download Excel</button></a>'
+
     # flask needs to return something that the web browser can understand (txt/html)
-    return df.to_html() + back_button
+    return df.to_html() + back_button + download_button
+
+# user wants to download results to excel
+@app.route('/download_excel')
+def download_excel():
+    # sends file created in /upload to send from server to computer
+    return send_file('temp_results.xlsx', as_attachment=True, download_name='heat_sheet_results.xlsx')
 
 app.run()
